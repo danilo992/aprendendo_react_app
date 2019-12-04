f4e919796a2a64907af51a729b1472e00914f020
Danilo-992 <48498808+danilo992@users.noreply.github.com>
2019-12-04 15:31:02 -0300

    Criando formuláiro pra entra na pagina admistrativ

--- my_app_react/src/App.js
+++ my_app_react/src/App.js
@@ -1,23 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+//import  imagen from './img/background.jpg'
+
 
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
+        <form className="formulario">
+        <h2>
+          Área de Cadastro!  Login 
+        </h2>
+          <p>Name:</p>
+            <input 
+              type="text" 
+              className="nome" 
+              placeholder="Nome Completo" 
+              required
+            />
+          <p>Email:</p>
+            <input 
+              type="email" 
+              className="email" 
+              placeholder="Email Principal" 
+              required
+            />
+          <p>Senha:</p>
+            <input 
+              id="password"
+              type="password" 
+              className="password" 
+              placeholder="senha de 8 dígitos" 
+            //  value={password}
+              required
+            /> <br />
+            <input 
+              type="submit" 
+              className="btn" 
+              placeholder="enviar" 
+            />
+        </form>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Site &#174; <a href="admistrador.js">www.progrmadorz.com.br</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p id="paragrafo">
+          Cadastre-se e receba promoções e descontos.
+        </p>
+        <p id="paragrafo">
+          Esse site é para todos que deseja contratar um 
+          <strong> programdor! </strong>.E para quem busca conhecimento na área 
+          da ciência da computação.
+        </p>
+       
       </header>
     </div>
   );
